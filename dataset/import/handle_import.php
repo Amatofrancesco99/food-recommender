c9139399338d9e0fac4cbe61f2e8577fce7a07cf
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2022-02-16 12:22:21 +0100

    Moved to localhost

--- dataset/import/handle_import.php
+++ dataset/import/handle_import.php
@@ -9,7 +9,7 @@
             if (($row > 1) && ($row % 2 == 0)) {
                 $sql = "INSERT INTO recipes (id, name, aver_rate, img_url, review_nums, ingredients, cooking_directions, nutritions, reviews) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
                 if($stmt = mysqli_prepare($con, $sql)){
-                    mysqli_stmt_bind_param($stmt, "isdsissss", $data[0], $data[1], $data[2], $data[3], $data[4], $data[5], $data[6], $data[7], $data[8].chr(34));
+                    mysqli_stmt_bind_param($stmt, "isdsissss", $data[0], $data[1], $data[2], $data[3], $data[4], $data[5], $data[6], $data[7], $data[8]);
                     if(!mysqli_stmt_execute($stmt)){
                         array_push($row_error, $data[0]);
                     }

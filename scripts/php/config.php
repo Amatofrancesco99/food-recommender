dcba4e07fd78f51ba6b42da8cb87d0aa4aaab983
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2022-02-16 11:41:51 +0100

    Added a brief description, if a user is interested on changing db coordinates

--- scripts/php/config.php
+++ scripts/php/config.php
@@ -1,8 +1,9 @@
 <?php    
+    /* Change this coordinates if you are interested on using a different MySQL database, located in another
+       "position" (example: localhost, or another online database) */
     $host = "sql11.freesqldatabase.com";
     $user = "sql11472048";
     $password = "EpBtK2wiRH";
     $dbname = "sql11472048";
-
     $con = mysqli_connect($host, $user, $password,$dbname);
 ?>

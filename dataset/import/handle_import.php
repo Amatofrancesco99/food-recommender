0ed810b80c5df65f8db9382526b89f9720efe507
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2022-02-16 11:51:05 +0100

    Show the food id that gives problem when importing, not the row, so that it can be more understandable

--- dataset/import/handle_import.php
+++ dataset/import/handle_import.php
@@ -11,7 +11,7 @@
                 if($stmt = mysqli_prepare($con, $sql)){
                     mysqli_stmt_bind_param($stmt, "isdsissss", $data[0], $data[1], $data[2], $data[3], $data[4], $data[5], $data[6], $data[7], $data[8].chr(34));
                     if(!mysqli_stmt_execute($stmt)){
-                        array_push($row_error, $row);
+                        array_push($row_error, $data[0]);
                     }
                     mysqli_stmt_close($stmt);
                 }

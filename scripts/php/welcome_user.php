8df1a6e1159b44788d5cadb4f94edc41e754dd88
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2022-02-17 12:52:46 +0100

    Little refactoring

--- scripts/php/welcome_user.php
+++ scripts/php/welcome_user.php
@@ -11,13 +11,12 @@
                 echo '<script type="text/javascript">',
                 'Swal.fire("Welcome '.ucfirst((htmlspecialchars($_SESSION['username']))).'", "It\'s appreciated if you leave a review after trying this app.", "success")',
                 '</script>';
-                $_SESSION['already_welcome'] = true;
             } else {
                 echo '<script type="text/javascript">',
                 'Swal.fire("Welcome back '.ucfirst((htmlspecialchars($_SESSION['username']))).'", "It\'s a pleasure having you there once again.", "success")',
                 '</script>';
-                $_SESSION['already_welcome'] = true;
             }
+            $_SESSION['already_welcome'] = true;
         }
 	}
 ?>
